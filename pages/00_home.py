116a083e2b0504819aaf215a5c994c59211f8213
Jie-Yan094 <jarita0904@gmail.com>
2025-11-19 03:17:29 +0000

    new file:   lake1.webp pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -2,28 +2,11 @@ import solara
 
 @solara.component
 def Page():
-    
-    solara.Markdown("### 水平排列 (Row)")
+    # 設定瀏覽器標籤頁的標題
+    solara.Title("我的 Solara WebGIS App")
+    solara.Markdown("### **花蓮馬太鞍溪堰塞湖 WebGIS 應用程式**")
+    solara.Markdown("""
+    這是一個使用 Solara 框架開發的 WebGIS 應用程式，展示了花蓮馬太鞍溪堰塞湖的地理資訊。
+    """)
+    solara.Image("https://example.com/hualien_barrier_lake_image.jpg", alt="花蓮堰塞湖圖片", width="600px")
 
-    # 'with' 語法會自動將內部的所有元件放入 Row 容器
-    with solara.Row():
-        solara.Markdown("欄位 1", style={"width": "30%", "border": "1px solid red"})
-        solara.Markdown("欄位 2", style={"width": "70%", "border": "1px solid blue"})
-
-    solara.Markdown("---")
-    
-    solara.Markdown("### 垂直排列 (Column)")
-    
-    # 'with' 語法會自動將內部的所有元件放入 Column 容器
-    with solara.Column():
-        solara.Markdown("這個元件在 Row 的下面")
-        solara.Markdown("這個元件在更下面")
-    
-    solara.Markdown("### 這是另一個置中的垂直排列容器 (Column)")
-
-    # 'with' 語法會自動將內部的所有元件放入 Column 容器
-    with solara.Column(align="center"):
-        solara.Markdown("## 顯示 DataFrame")
-        solara.DataFrame(df)
-        solara.Markdown("## 顯示 Plotly 圖表")
-        solara.FigurePlotly(fig)
